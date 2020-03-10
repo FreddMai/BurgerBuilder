93e7717bd19d79b568b612f115f2c0a09aafe0fe
Freddy M <fredtein@gmail.com>
2020-03-10 10:54:31 +0200

    Improved some small things

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,9 +4,7 @@ import Aux from '../../../hoc/Auxilirary/Auxilirary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component{
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
-    }
+
     render(){
         const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
