908c5b3d9cc5fdf119b84ab7e2ef84f8ea4737d0
Freddy M <fredtein@gmail.com>
2020-03-20 15:02:04 +0200

    Adding react memo and custom error hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,37 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxilirary/Auxilirary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return props => {
-        const [error, setError] = useState(null);
-
-        const reqInter = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        })
-        const resInter = axios.interceptors.response.use(res => res, err => {
-            setError(err);
-        });
-
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInter);
-                axios.interceptors.response.eject(resInter);
-            }
-        }, [reqInter, resInter]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
-
+        const [error, clearError] = useHttpErrorHandler(axios);
             return (
                 <Aux>
                     <Modal 
                         show={error}
-                        modalClosed={errorConfirmedHandler}>
-                        {error ? error.message : null}
+                        modalClosed={clearError}>
+                            {error ? error.message : null}
                     </Modal>
                     <WrappedComponent {...props} />
                 </Aux>
